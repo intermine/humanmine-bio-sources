0f99e9c82ba2135215d5321b73596c3ebf655e74
AdrianBZG <adrianrodriguezbazaga@gmail.com>
2021-05-18 17:47:20 +0100

    Update copyright notice + Move constructor to be the first method + Removed gene list functionality

--- disgenet-disease-associations/src/main/java/org/intermine/bio/dataconversion/DisgenetDiseaseAssociationsConverter.java
+++ disgenet-disease-associations/src/main/java/org/intermine/bio/dataconversion/DisgenetDiseaseAssociationsConverter.java
@@ -1,7 +1,7 @@
 package org.intermine.bio.dataconversion;
 
 /*
- * Copyright (C) 2002-2018 FlyMine
+ * Copyright (C) 2002-2020 FlyMine
  *
  * This code may be freely distributed and modified under the
  * terms of the GNU Lesser General Public Licence.  This should
@@ -37,61 +37,13 @@ public class DisgenetDiseaseAssociationsConverter extends BioDirectoryConverter
     //
     private static final String DATASET_TITLE = "DisGeNET";
     private static final String DATA_SOURCE_NAME = "Curated gene-disease associations (v7 - June 2020)";
-
     private static final String TAXON_ID = "9606"; // Human Taxon ID
-
     private static final String DISGENET_FILE = "curated_gene_disease_associations.tsv";
-
     private Map<String, String> genes = new HashMap<String, String>();
     private Map<String, Item> diseases = new HashMap<String, Item>();
-
     protected IdResolver rslv;
     private static final Logger LOG = Logger.getLogger(DisgenetDiseaseAssociationsConverter.class);
-
-    private String organismIdentifier; // Not the taxon ID. It references the object that is created into the database.
-
-    // Methods to integrate the data only for a list of genes - Empty string: All genes
-    private static final String GENE_LIST_FILE = "";
-    private ArrayList<String> processGeneList(String geneListFile) throws Exception {
-        File geneListF = new File(geneListFile);
-
-        Iterator<?> lineIter = FormattedTextParser.parseCsvDelimitedReader(new FileReader(geneListF));
-        ArrayList<String> geneListArray = new ArrayList<String>();
-
-        while (lineIter.hasNext()) {
-            String[] line = (String[]) lineIter.next();
-            String gene = line[0];
-            if(StringUtils.isEmpty(gene)) {
-                continue;
-            }
-
-            String resolvedGeneIdentifier = getGeneIdentifier(gene);
-            if(resolvedGeneIdentifier != null) {
-                geneListArray.add(resolvedGeneIdentifier);
-            }
-        }
-
-        return geneListArray;
-    }
-
-    private String getGeneIdentifier(String geneSymbol) throws ObjectStoreException {
-        String resolvedIdentifier = resolveGene(geneSymbol);
-        if (StringUtils.isEmpty(resolvedIdentifier)) {
-            return null;
-        }
-        String geneId = genes.get(resolvedIdentifier);
-        if (geneId == null) {
-            Item gene = createItem("Gene");
-            gene.setAttribute("primaryIdentifier", resolvedIdentifier);
-            //gene.setAttribute("symbol", primaryIdentifier);
-            //gene.setReference("organism", getOrganism(TAXON_ID));
-            store(gene);
-            geneId = gene.getIdentifier();
-            genes.put(resolvedIdentifier, geneId);
-        }
-        return geneId;
-    }
-    //
+    private String organismIdentifier;
 
     /**
      * Constructor
@@ -105,28 +57,13 @@ public class DisgenetDiseaseAssociationsConverter extends BioDirectoryConverter
         }
     }
 
-    /*@Override
-    public void close() throws Exception {
-        store(diseases.values());
-    }*/
-
-    /**
-     * 
-     *
-     * {@inheritDoc}
-     */
     public void process(File dataDir) throws Exception {
 
         Map<String, File> files = readFilesInDir(dataDir);
 
         organismIdentifier = getOrganism(TAXON_ID);
 
-        ArrayList<String> geneListArray = new ArrayList<String>();
-        if(!StringUtils.isEmpty(GENE_LIST_FILE)) {
-            geneListArray = processGeneList(GENE_LIST_FILE);
-        }
-
-        processAssociations(new FileReader(files.get(DISGENET_FILE)), geneListArray);
+        processAssociations(new FileReader(files.get(DISGENET_FILE)));
 
     }
 
@@ -138,7 +75,7 @@ public class DisgenetDiseaseAssociationsConverter extends BioDirectoryConverter
         return files;
     }
 
-    private void processAssociations(Reader reader, ArrayList<String> geneList) throws ObjectStoreException, IOException {
+    private void processAssociations(Reader reader) throws ObjectStoreException, IOException {
         Iterator<?> lineIter = FormattedTextParser.parseTabDelimitedReader(reader);
         // Skip header
         lineIter.next();
@@ -147,18 +84,12 @@ public class DisgenetDiseaseAssociationsConverter extends BioDirectoryConverter
             String[] line = (String[]) lineIter.next();
 
             String geneSymbol = line[1];
-
             String resolvedGene = getGeneIdentifier(geneSymbol);
-            if(!geneList.isEmpty() && !geneList.contains(resolvedGene)) {
-                continue;
-            }            
-
             String diseaseId = line[4];
             String diseaseName = line[5];
             String diseaseType = line[6];
             String score = line[9];
 
-            //Item disease = getDisease(diseaseId);
             Item disease = null;
 
             if (diseases.get(diseaseId) == null) {
@@ -197,6 +128,24 @@ public class DisgenetDiseaseAssociationsConverter extends BioDirectoryConverter
         }
     }
 
+    private String getGeneIdentifier(String geneSymbol) throws ObjectStoreException {
+        String resolvedIdentifier = resolveGene(geneSymbol);
+        if (StringUtils.isEmpty(resolvedIdentifier)) {
+            return null;
+        }
+        String geneId = genes.get(resolvedIdentifier);
+        if (geneId == null) {
+            Item gene = createItem("Gene");
+            gene.setAttribute("primaryIdentifier", resolvedIdentifier);
+            //gene.setAttribute("symbol", primaryIdentifier);
+            //gene.setReference("organism", getOrganism(TAXON_ID));
+            store(gene);
+            geneId = gene.getIdentifier();
+            genes.put(resolvedIdentifier, geneId);
+        }
+        return geneId;
+    }
+
     private String getGeneId(String primaryIdentifier) throws ObjectStoreException {
         String resolvedIdentifier = resolveGene(primaryIdentifier);
         if (StringUtils.isEmpty(resolvedIdentifier)) {
@@ -206,8 +155,6 @@ public class DisgenetDiseaseAssociationsConverter extends BioDirectoryConverter
         if (geneId == null) {
             Item gene = createItem("Gene");
             gene.setAttribute("symbol", primaryIdentifier);
-            //gene.setAttribute("symbol", primaryIdentifier);
-            //gene.setReference("organism", getOrganism(TAXON_ID));
             store(gene);
             geneId = gene.getIdentifier();
             genes.put(primaryIdentifier, geneId);
